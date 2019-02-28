217768f0f8a29b68429ca38612fc21321c0e1120
Benjamin Clark <benjaminclark@Benjamins-Mac-mini.local>
2019-03-01 08:02:22 +1030

    Add basic server-side cache

--- client/src/actions/actions.ts
+++ client/src/actions/actions.ts
@@ -1,5 +1,5 @@
 import { Fn } from "react-fn";
-import { StateNode, State_Restaurants, State_Categories, State_Cuisines } from "../state/state";
+import { StateNode, State_Zomato_Basic_List } from "../state/state";
 import { $fetch } from "../../utils/fetch/fetch";
 
 export interface Actions {
@@ -9,16 +9,14 @@ export interface Actions {
 }
 export const actions = (fn: Fn): Actions => ({
     getRestaurants: async () => {
+        fn.updateState(stateNode('restaurants/firstFetch'), true, { rerender: false });
         try {
             const result = await $fetch('http://localhost:3000/restaurants', {
                 method: 'get'
             });
             return fn.updateState(
-                stateNode('restaurants'), 
-                stateValue<State_Restaurants>({
-                    firstFetch: true,
-                    list: result.message
-                })
+                stateNode('restaurants/list'), 
+                stateValue<State_Zomato_Basic_List>(result.message)
             );
         } catch (e) {
             console.error('Unable to fetch restaurants');
@@ -26,16 +24,14 @@ export const actions = (fn: Fn): Actions => ({
         }
     },
     getCategories: async () => {
+        fn.updateState(stateNode('categories/firstFetch'), true, { rerender: false });
         try {
             const result = await $fetch('http://localhost:3000/categories', {
                 method: 'get'
             });
             return fn.updateState(
-                stateNode('categories'), 
-                stateValue<State_Categories>({
-                    firstFetch: true,
-                    list: result.message
-                })
+                stateNode('categories/list'), 
+                stateValue<State_Zomato_Basic_List>(result.message)
             );
         } catch (e) {
             console.error('Unable to fetch categories');
@@ -43,16 +39,14 @@ export const actions = (fn: Fn): Actions => ({
         }
     },
     getCuisines: async () => {
+        fn.updateState(stateNode('cuisines/firstFetch'), true, { rerender: false });
         try {
             const result = await $fetch('http://localhost:3000/cuisines', {
                 method: 'get'
             });
             return fn.updateState(
-                stateNode('cuisines'), 
-                stateValue<State_Cuisines>({
-                    firstFetch: true,
-                    list: result.message
-                })
+                stateNode('cuisines/list'), 
+                stateValue<State_Zomato_Basic_List>(result.message)
             );
         } catch (e) {
             console.error('Unable to fetch cuisines');
--- client/src/state/state.tsx
+++ client/src/state/state.tsx
@@ -1,25 +1,22 @@
-export type StateNode = 'greeting' | 'restaurants' | 'categories' | 'cuisines';
+export type StateNode = 'greeting' | 'restaurants/list' | 'restaurants/firstFetch' | 
+'categories/list' | 'categories/firstFetch' | 'cuisines/list' | 'cuisines/firstFetch';
 
-export interface State_Restaurants {
-    firstFetch: boolean,
-    list: { name: string, id: string }[]
-}
-
-export interface State_Categories {
-    firstFetch: boolean,
-    list: { name: string, id: string }[]
-}
-
-export interface State_Cuisines {
-    firstFetch: boolean,
-    list: { name: string, id: string }[]
-}
+export type State_Zomato_Basic_List = { name: string, id: string }[]
 
 export interface State {
     route: string[],
-    restaurants: State_Restaurants,
-    categories: State_Categories,
-    cuisines: State_Cuisines
+    restaurants: {
+        firstFetch: boolean,
+    list: State_Zomato_Basic_List
+    },
+    categories: {
+        firstFetch: boolean,
+        list: State_Zomato_Basic_List
+    },
+    cuisines: {
+        firstFetch: boolean,
+        list: State_Zomato_Basic_List
+    }
 }
 
 export const state: State = {
